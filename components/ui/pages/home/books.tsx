82c11d735793e2a23f8054618e3f781ce01d6b71
billalxcode <billal.xcode@gmail.com>
2025-02-01 11:26:44 +0700

    feat: add BooksWrapper component for displaying book-related content

---
+++ components/ui/pages/home/books.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function BooksWrapper() {
+  return (
+    <div className="w-full bg-card p-4 rounded-xl">
+        <p className='font-bold'>Books Wrapper</p>
+    </div>
+  )
+}
