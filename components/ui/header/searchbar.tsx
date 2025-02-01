b6c656fa87f4b9e82ffa3e4e6bc5f79c45ddd42b
billalxcode <billal.xcode@gmail.com>
2025-02-01 11:26:39 +0700

    feat: add SearchBar component for book search functionality in the header

---
+++ components/ui/header/searchbar.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { CgSearch } from 'react-icons/cg'
+
+export default function SearchBar() {
+  return (
+    <div className="flex flex-row gap-4 w-1/2 items-center bg-white px-4 p-2 rounded-md">
+        <CgSearch size={20} className='text-foreground' />
+
+        <input
+            type="text"
+            placeholder="Cari buku agama islam"
+            className="bg-transparent border-none outline-none w-full font-semibold"
+        />
+    </div>
+  )
+}
