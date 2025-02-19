73ff0e0622955a1063230dd16be5d356e0119e0d
billalxcode <billal.xcode@gmail.com>
2025-02-19 16:18:13 +0700

    feat: implement DropdownMenu in NavProfile for account options

--- components/ui/header/profile.tsx
+++ components/ui/header/profile.tsx
@@ -1,16 +1,42 @@
-import Image from 'next/image'
-import React from 'react'
+import Image from "next/image";
+import React from "react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "../dropdown-menu";
+import Link from "next/link";
 
 export default function NavProfile() {
   return (
-    <div className="flex flex-col items-center justify-center p-[2px] border-2 border-primary rounded-full cursor-pointer">
-      <Image
-          src={"/avatars/1.jpg"}
-          alt='profile'
-          width={30}
-          height={30}
-          className='rounded-full'
-      />
-    </div>
-  )
+    <DropdownMenu>
+      <DropdownMenuTrigger>
+        <div className="flex flex-col items-center justify-center p-[2px] border-2 border-primary rounded-full cursor-pointer select-none">
+          <Image
+            src={"/avatars/1.jpg"}
+            alt="profile"
+            width={30}
+            height={30}
+            className="rounded-full"
+          />
+        </div>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        <DropdownMenuLabel className="font-semibold">
+          My Account
+        </DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem>
+          <Link href={"/profile"}>Profile</Link>
+        </DropdownMenuItem>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem>
+          <Link href={"/logout"}>Logout</Link>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
 }
