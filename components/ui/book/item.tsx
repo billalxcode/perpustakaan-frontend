d831e9ff8be5923676ce45a11f67b66f0ae47dec
billalxcode <billal.xcode@gmail.com>
2025-02-19 16:17:15 +0700

    feat: enhance BookItem component with image and book details

--- components/ui/book/item.tsx
+++ components/ui/book/item.tsx
@@ -1,7 +1,25 @@
-import React from 'react'
+import Image from "next/image";
+import React from "react";
 
 export default function BookItem() {
   return (
-    <div>book</div>
-  )
+    <div className="flex flex-col gap-4 p-2 bg-accent rounded-xl">
+      <Image
+        src={"/books/cover/254uuwz95w.jpg"}
+        alt="book cover"
+        width={150}
+        height={150}
+        className="rounded-xl"
+      />
+
+      <div className="mb-2 mx-2">
+        <p className="text-[11px] text-gray-500">F. Scott Fitzgerald</p>
+        <h1 className="text-sm font-bold">The Great Gatsby</h1>
+        <div className="flex items-center mt-2 gap-1">
+          <span className="text-yellow-500">★★★★☆</span>
+          <span className="text-xs text-gray-500">(4.0)</span>
+        </div>
+      </div>
+    </div>
+  );
 }
