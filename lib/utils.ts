830edded20d6a4275a9d9589a9ededcf7641ed7a
billalxcode <billal.xcode@gmail.com>
2025-02-19 21:02:59 +0700

    feat: add axios instance for API requests with default headers

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,15 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import axios from "axios";
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+export const request = axios.create({
+  baseURL: "http://localhost/api",
+  headers: {
+    "Content-Type": "application/json",
+    Accept: "application/json",
+  },
+});
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
