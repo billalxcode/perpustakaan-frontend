38c472c347c3f4e1b99417ded20724b75f7e2d8d
billalxcode <billal.xcode@gmail.com>
2025-02-19 20:17:58 +0700

    feat: update ProfileWrapper to display user information or authentication options based on login status

--- components/ui/pages/home/profile.tsx
+++ components/ui/pages/home/profile.tsx
@@ -1,25 +1,49 @@
+"use client"
 import Image from "next/image";
 import React from "react";
 import { Badge } from "../../badge";
+import useAuth from "@/states/features/auth/hooks";
+import { Button } from "../../button";
 
 export default function ProfileWrapper() {
+  const { isAuthenticated } = useAuth();
+
   return (
     <div className="relative w-1/6">
       <div className="flex flex-col flex-1 gap-3 items-center text-card-foreground bg-card p-4 rounded-xl">
-        <Image
-          src={"/avatars/1.jpg"}
-          alt="profile"
-          width={100}
-          height={100}
-          className="rounded-full"
-        />
+        {
+          isAuthenticated ? (
+            <>
+              <Image
+                src={"/avatars/1.jpg"}
+                alt="profile"
+                width={100}
+                height={100}
+                className="rounded-full"
+              />
 
-        <div className="flex flex-col gap-0 items-center w-1/2 text-justify">
-          <div className="text-lg font-bold text-center">John Doe</div>
-          <Badge className="rounded-full w-full p-2 mx-auto">
-            Anime Lover
-          </Badge>
-        </div>
+              <div className="flex flex-col gap-0 items-center w-1/2 text-justify">
+                <div className="text-lg font-bold text-center">John Doe</div>
+                <Badge className="rounded-full w-full p-2 mx-auto">
+                  Anime Lover
+                </Badge>
+              </div>
+            </>
+          ) : (
+            <>
+              <div className="text-lg font-bold text-center">Welcome</div>
+
+              <Button className="w-full">
+                Register
+              </Button>
+              
+              <Button className="w-full">
+                Login
+              </Button>
+
+            </>
+          )
+        }
       </div>
     </div>
   );
