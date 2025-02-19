d62effb0a2e95aefdcb37a13107475fa38312ff4
billalxcode <billal.xcode@gmail.com>
2025-02-19 16:18:39 +0700

    feat: enhance ProfileWrapper layout with improved structure and Badge component

--- components/ui/pages/home/profile.tsx
+++ components/ui/pages/home/profile.tsx
@@ -1,22 +1,25 @@
 import Image from "next/image";
 import React from "react";
+import { Badge } from "../../badge";
 
 export default function ProfileWrapper() {
   return (
-    <div className="flex flex-col gap-3 items-center text-card-foreground w-1/2 bg-card p-4 rounded-xl">
-      <Image
-        src={"/avatars/1.jpg"}
-        alt="profile"
-        width={100}
-        height={100}
-        className="rounded-full"
-      />
+    <div className="relative w-1/6">
+      <div className="flex flex-col flex-1 gap-3 items-center text-card-foreground bg-card p-4 rounded-xl">
+        <Image
+          src={"/avatars/1.jpg"}
+          alt="profile"
+          width={100}
+          height={100}
+          className="rounded-full"
+        />
 
-      <div className="flex flex-col gap-0 items-center w-1/2 text-justify">
-        <h1 className="text-lg font-bold">John Doe</h1>
-        <p className="text-sm font-semibold bg-primary text-primary-foreground p-1 px-2 rounded-full">
-          Anime Lover
-        </p>
+        <div className="flex flex-col gap-0 items-center w-1/2 text-justify">
+          <div className="text-lg font-bold text-center">John Doe</div>
+          <Badge className="rounded-full w-full p-2 mx-auto">
+            Anime Lover
+          </Badge>
+        </div>
       </div>
     </div>
   );
