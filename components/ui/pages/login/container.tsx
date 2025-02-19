a15402c46cde121b27f997e06a79912c7223b719
billalxcode <billal.xcode@gmail.com>
2025-02-19 20:18:02 +0700

    feat: add LoginContainer component for user login interface

---
+++ components/ui/pages/login/container.tsx
@@ -0,0 +1,9 @@
+export default function LoginContainer() {
+  return (
+    <div className="p-4 flex flex-row gap-4 w-full min-h-screen justify-center items-center">
+      <div className="w-4 bg-card p-4 rounded-xl flex-1">
+      
+      </div>
+    </div>
+  );
+}
