af04ef00923fd0a874999c34f9f686a2ba0d6981
billalxcode <billal.xcode@gmail.com>
2025-02-19 16:18:09 +0700

    feat: add Footer component with copyright and author information

---
+++ components/ui/footer/footer.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <footer className="text-muted-foreground p-4 flex flex-row justify-between px-12">
+        <p>
+            &copy; 2024 <strong className='font-[700]'>BukuKita.ID</strong>
+        </p>
+        <p>
+            Made with ❤️ by <a href="https://github.com/billalxcode" className='font-bold text-primary'>Billal Fauzan</a>
+        </p>
+    </footer>
+  )
+}
